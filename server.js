90d403bc73317a09dcbeaff848f7ca9be1d48536
manish <manishpanwar682@gmail.com>
2022-11-19 23:53:21 -0800

    hello

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const port = process.env.PORT || 5000
 app.use(express.json());
 app.use(cors()) 
 app.use('/',todolist);
-
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
 });
