b8308051f9799d20558cf28580744f064d81f806
manish <manishpanwar682@gmail.com>
2022-11-19 22:52:09 -0800

    hello

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ require("dotenv").config();
 const database = require("./DB/mongoose");
 database()
 const app = express();
-const register= require('./router/register')
+const register= require('./router/register');
 const todolist = require("./router/Todo");
-var cors = require('cors')
-app.use(express.json())
+var cors = require('cors');
+app.use(express.json());
 app.use(cors()) 
 app.use('/',register);
 app.use('/',todolist);
